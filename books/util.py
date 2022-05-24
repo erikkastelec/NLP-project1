edf7a1e57c537c89b00ce0084f8127ad33f683b6
Matic Isovski <mi6568@student.uni-lj.si>
2022-05-24 20:35:53 +0200

    refactor & corpus fix

--- books/util.py
+++ books/util.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 import numpy as np
 
 
@@ -12,28 +12,49 @@ def vocabulary(filename):
         return len(unique), len(tokens)
     
 
-def avg_vocabulary(corpus, language):
+def avg_vocabulary(corpus, language='both'):
     type_token_ratio = 0
     avg_V = 0
-    filtered = corpus[corpus['language'] == language]
+    avg_N = 0
+    if language == 'both':
+        filtered = corpus
+    else:
+        filtered = corpus[corpus['language'] == language]
     for filename in filtered['filename']:
         V, N = vocabulary(filename)
         avg_V += V
+        avg_N += N
         type_token_ratio += V/N
 
-    type_token_ratio /= len(filtered)
-    avg_V /= len(filtered)
-
-    return type_token_ratio, avg_V
+    return type_token_ratio / len(filtered), avg_V / len(filtered), avg_N / len(filtered)
 
 
-def avg_attribute_num(corpus, attr, language):
+def avg_attribute_num(corpus, attr, language='both'):
     counter = 0
+    cor = corpus
+    if language != 'both':
+        cor = corpus[corpus['language'] == language]
+    
     if type(attr) == str:
-        filtered = corpus[corpus['language'] == language][attr]
+        filtered = cor[attr]
     else:
-        filtered = corpus[corpus['language'] == language].iloc[:, attr]
+        filtered = cor.iloc[:, attr]
+
     for r in filtered:
         counter += len(r.split(', '))
     
     return counter / len(filtered)
+
+def avg_word_in_sentences(corpus, language='both'):
+    if language == 'both':
+        filtered = corpus
+    else:
+        filtered = corpus[corpus['language'] == language]
+    st_ratio = 0
+    for filename in filtered['filename']:
+        with open(filename, encoding="utf8") as f:
+            contents = f.read()
+            sentences = sent_tokenize(contents)
+            tokens = word_tokenize(contents)
+            st_ratio += len(tokens) / len(sentences)
+    return st_ratio / len(filtered)
